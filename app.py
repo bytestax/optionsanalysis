09b0d0bb0771362b9b2a8baa48f0a1de14935ac0
bytestax <41375884+bytestax@users.noreply.github.com>
2025-08-27 14:41:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 # ==============================
 # Polygon API Setup
 # ==============================
-API_KEY = "YOUR_POLYGON_API_KEY"
+API_KEY = "GuHtoE7JtmzxOpLU_yL_RQOnF1Leliqw"
 BASE_URL = "https://api.polygon.io"
 
 
